6a6432649f47f83ebc5644d7cc970ae1742accb7
amaziane <amaziane.n@gmail.com>
2023-05-16 19:13:40 +0200

    fix binding  exercice

--- src/app/formation/formation-list/formation-list.component.ts
+++ src/app/formation/formation-list/formation-list.component.ts
@@ -5,14 +5,31 @@ import { Component } from "@angular/core";
   templateUrl:'formation-list.component.html'
 })
 export class FormationsList {
-  toggle() {
-    this.activer = !this.activer;
-  }
-  activer:boolean = true;
   title = 'liste Formation';
- trainings = [
-    {nom:'Formation Angular Débutant', date: '15/02/2023', heure: '18h', url:"https:team.com"},
-    {nom:'Formation Angular Avancé', date: '15/06/2023', heure: '18h', lieu: {adresse: 'adresse-1', ville: 'ville-1', pays: 'pays-1'}},
-    {nom:'AWS', date: '15/04/2023', heure: '18h'}
-]
+  trainings = [
+    {
+      nom: 'Formation Angular Débutant', date: '15/02/2023', heure: '18h', url:
+        "https:team.com", format: "enligne", hidden: false
+    },
+    {
+      nom: 'Formation Angular Avancé', date: '15/06/2023', heure: '18h', lieu: {
+        adresse:
+          'adresse-1', ville: 'ville-1', pays: 'pays-1'
+      }, format: "sursite", hidden: false
+    },
+    {nom: 'AWS', date: '15/04/2023', heure: '18h', hidden: false}
+  ];
+  toggleAll() {
+   this.trainings.forEach(item => {
+        item.hidden = !item.hidden;
+    });
+  }
+  toggle(nom:any) {
+    this.trainings.filter(it=>it.nom === nom).forEach(item => {
+      if (item.nom === nom)
+        item.hidden = !item.hidden;
+    });
+  }
+ 
+  
 }
